7258811574bbc6cf7a5ba5d485f1abb59b2241e2
vitor Luiz de Souza <vitor.souza270903@gmail.com>
2024-02-07 15:43:55 -0300

    Chute o número totalmente terminado

--- chute_numero.py
+++ chute_numero.py
@@ -9,53 +9,94 @@ class Num_aleat:
         self.msg_menor = 'Tente um número menor'
         self.msg_maior = 'Tente um número maior'
         self.msg_certo = 'VOCÊ ACERTOU O NÚMERO!'
-        self.loop = True
+        #self.loop = True
         
     def Gerar_num(self):
         #gerar o guardar número
         self.num = random.randint(self.menor, self.maior)
         
     def Reiniciar(self):
-        print('\nGostaria de Reiniciar novamente? ')
-        while (self.loop is True):
-            self.resp = input()
-            if(self.resp == 'sim' or self.resp == 's'):
-                self.loop = False
-                self.Iniciar()
-            elif(self.resp == 'não' or self.resp == 'n'):
-                self.loop = False
+        """self.msg['text'] = '\nGostaria de Reiniciar novamente? '
+        #while (self.loop is True):
+            #self.Input()  
+        if(self.resp == 'sim' or self.resp == 's'):
+            #self.loop = False
+            self.msg['text'] = 'Um número de 0 a 25 foi gerado aleatóriamente. Tente acertar qual o número: '
+            self.resultado['text'] = ''
+        elif(self.resp == 'não' or self.resp == 'n'):
+            #self.loop = False
+            self.layout.destroy()
+        else:
+            self.resultado['text'] = 'Por Favor, Digite "sim" ou "não"'"""
+        self.layout2 = Tk()
+        self.layout2.title('Reiniciar?')
+        
+        pergunta = Label(self.layout2, text='Deseja adivinhar o número denovo?')
+        pergunta.grid(row=0, column=0)
+        botao_sim = Button(self.layout2, text='sim', command=self.Com_sim)
+        botao_sim.grid(row=1, column=0)
+        botao_nao = Button(self.layout2, text='não', command=self.Com_nao)
+        botao_nao.grid(row=1, column=1)
+                
+        self.layout2.mainloop()
+        
+    def Com_sim(self):
+        self.layout.destroy()
+        self.layout2.destroy()
+        self.Iniciar()
+        
+    def Com_nao(self):
+        self.layout.destroy()
+        self.layout2.destroy()
+        
+    def Inp(self):
+        try:
+            if int(self.resp.get('1.0', END)) == self.num:
+                self.resultado['text'] = self.msg_certo
+                self.Reiniciar()
+            elif int(self.resp.get('1.0', END)) > self.num and int(self.resp.get('1.0', END)) <= 25:
+                self.resultado['text'] = self.msg_menor
+            elif int(self.resp.get('1.0', END)) < self.num and int(self.resp.get('1.0', END)) >= 0:
+                self.resultado['text'] = self.msg_maior
             else:
-                print('Por Favor, Digite "sim" ou "não"')
+                self.resultado = 'Por favor, digite um número de 0 a 25'
+        except:
+            self.resultado = 'ERRO: Por favor, digite apenas números'
              
     def Iniciar(self):
-        self.loop = True
+        #self.loop = True
         self.Gerar_num()
-        print(self.msg)
+        print(self.num)
+        #print(self.msg)
         #responder de acordo com o usuario
-        while (self.loop is True):
-            self.resp = input()
+        """while (self.loop is True):
             try:
                 if int(self.resp) == self.num:
-                    print(self.msg_certo)
+                    self.resultado['text'] = self.msg_certo
                     self.Reiniciar()
                 elif int(self.resp) > self.num and int(self.resp) <= 25:
-                    print(self.msg_menor)
+                    self.resultado['text'] = self.msg_menor
                 elif int(self.resp) < self.num and int(self.resp) >= 0:
-                    print(self.msg_maior)
+                    self.resultado['text'] = self.msg_maior
                 else:
-                    print('Por favor, digite um número de 0 a 25')
+                    self.resultado = 'Por favor, digite um número de 0 a 25'
             except:
-                print('ERRO: Por favor, digite apenas números')
+                self.resultado = 'ERRO: Por favor, digite apenas números'"""
         
         #layout
-        layout = Tk()
-        layout.title('Acerte o Número')
+        self.layout = Tk()
+        self.layout.title('Acerte o Número')
         
-        txt_inicio = Label(layout, text=self.msg)
-        self.resp = Text(layout, height=5, width=20)
-        resultado = Label(layout, text='')
+        self.txt_inicio = Label(self.layout, text=self.msg)
+        self.txt_inicio.grid(row=0, column=0)
+        self.botao = Button(self.layout, text='Submit', command=self.Inp)
+        self.botao.grid(row=1,column=0)
+        self.resp = Text(self.layout, height=5, width=20)
+        self.resp.grid(row=2,column=0)
+        self.resultado = Label(self.layout, text='')
+        self.resultado.grid(row=3,column=0)
         
-        layout.mainloop()
-        
+        self.layout.mainloop()
+    
 gerar = Num_aleat()
 gerar.Iniciar()
