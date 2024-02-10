695c6469d9f042d6b14a1694c071b679727599b6
vitor Luiz de Souza <vitor.souza270903@gmail.com>
2024-02-10 00:18:36 -0300

    Começando jogo de escolhas

---
+++ jogo_de_aventura.py
@@ -0,0 +1,19 @@
+
+
+class Jogo:
+    def __init__(self):
+        self.introduçao = 'Você acorda em uma caverna sem se lembrar de nada, com uma mochila e uma espada ao seu lado e muita dor de cabeça. Também há outra pessoa desacordada logo a frente.\nO que você faz?\n'
+        self.pergunta1 = '\nTentar se lembrar'
+        self.pergunta2 = '\nDescobrir entrada da caverna'
+        self.pergunta3 = '\nVerificar pessoa'
+        self.pergunta4 = '\nAbrir mochila'
+        self.pergunta5 = '\nMatar-se'
+        self.inp = ''
+
+    def Iniciar(self):
+        while True:
+            input(self.inp)
+            if 
+        
+jogo = Jogo()
+jogo.Iniciar()
